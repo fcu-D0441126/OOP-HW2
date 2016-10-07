2954a8ecd30221b036d72be6f061425837386891
fcu-D0441126 <ywchenjason@gmail.com>
2016-10-07 09:32:03 +0800

    OOP-HW2
    HW2

---
+++ KeyboardScanner.java
@@ -0,0 +1,22 @@
+package fcu.iecs.oop;
+
+import java.util.Scanner;
+
+public class KeyboardScanner {
+
+	public static void main(String[] args) {
+		int integer;
+		float float_point_number;
+		String name;
+		Scanner scanf=new Scanner(System.in);
+		System.out.printf("Enter a integer: ");
+		integer=scanf.nextInt();
+		System.out.printf("Enter a float point number:");
+		float_point_number=scanf.nextFloat();
+		System.out.printf("Enter a you name:");
+		name=scanf.next();
+		System.out.printf("Hi %s, the multiplication of %d and %f is %e ", name , integer , float_point_number , integer*float_point_number );
+		scanf.close();
+	}
+
+}
