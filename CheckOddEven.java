89ed87ac71bf20c7ccbe7406f3c59317350aae84
fcu-D0441126 <ywchenjason@gmail.com>
2016-10-07 09:42:16 +0800

    OOP-HW2

--- CheckOddEven.java
+++ CheckOddEven.java
@@ -1,9 +1,21 @@
 package fcu.iecs.oop;
 
+import java.util.Scanner;
+
 public class CheckOddEven {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int integer;
+		Scanner scanf= new Scanner (System.in);
+		
+		System.out.printf("Enter a integer:");
+		integer= scanf.nextInt();
+		
+		if(integer%2==0) System.out.printf("The input integer is Even Number.");
+		else System.out.printf("The input integer is Odd Number.");
+		
+		scanf.close();
+	
 
 	}
 
