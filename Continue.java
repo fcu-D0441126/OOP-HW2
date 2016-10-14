418975c9d21e593b6153c03dbf956101a394aa12
fcu-D0441126 <ywchenjason@gmail.com>
2016-10-14 20:21:13 +0800

    Update Continue.java

--- Continue.java
+++ Continue.java
@@ -3,7 +3,7 @@ public class Continue {
 
 	public static void main(String[] args) {
 		for(int i = 0; i < 5; i ++) {
-			if(i%2==00) continue;
+			if(i%2==0) continue;
 			System.out.println(i);
 		}
 
